44333629ffe2c2e1ec9ade476f93b1d00dc26ff4
Adeboga Abigail <abbyboga@gmail.com>
2024-05-04 02:37:53 -0700

    Fix??

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
 #Insall flask from pip3
 
 exec { 'flask':
-  command => 'usr/bin/pip3 install Flask==2.1.0',
-  require => Package['flask-from-pip3']
-}
-
-package { 'flask-from-pip3':
-  ensure => 'installed'
+  command => 'usr/bin/pip3 install Flask==2.1.0'
 }
