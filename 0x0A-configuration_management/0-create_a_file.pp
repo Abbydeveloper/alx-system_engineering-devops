7ac8bf0d381e22ec7223def62e24f287200fa238
Adeboga Abigail <abbyboga@gmail.com>
2024-05-04 01:59:19 -0700

    Create a file with Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,14 @@
+$doc_root = "/tmp"
+
+file { $doc_root:
+	ensure => "directory",
+	owner => "www-data",
+	group => "www-data",
+	mode => 0744
+}
+
+file { "$doc_root/school":
+	ensure => "present",
+	content => "I love Puppet",
+	require => File[$doc_root]
+}
