576036e149802d403044c96c9a18b15b6b2601b3
Adeboga Abigail <abbyboga@gmail.com>
2024-05-04 02:36:38 -0700

    install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
 #Insall flask from pip3
 
-package { 'flask-from-pip3':
-  ensure => 'installed'
-}
-
 exec { 'flask':
   command => 'usr/bin/pip3 install Flask==2.1.0',
   require => Package['flask-from-pip3']
+}
+
+package { 'flask-from-pip3':
+  ensure => 'installed'
 }
