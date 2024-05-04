6baef97ec577d818409fad43618ccaa4821c16cb
Adeboga Abigail <abbyboga@gmail.com>
2024-05-04 03:29:06 -0700

    Fix??

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
 # install flask from pip3
+
+package { 'werkzeug':
+  ensure   => '2.2.2',
+  provider => 'pip3'
+}
+
 package { 'flask' :
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package[werkzeug]
 }
