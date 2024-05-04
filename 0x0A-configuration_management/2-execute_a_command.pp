e0f7fa83b2150f5bbf13d08d075056d431ac698d
Adeboga Abigail <abbyboga@gmail.com>
2024-05-04 03:47:46 -0700

    kill process killmen ow??

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-# kill process
+# kill process killmenow
 
-exec { 'pkill' :
-  command => 'pkill killmenow',
-  provier => 'shell'
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
