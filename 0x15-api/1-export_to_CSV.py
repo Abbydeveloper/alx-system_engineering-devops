a3d8c93bbc5922d58caa16c9985d4422e703de17
Adeboga Abigail <abbyboga@gmail.com>
2024-05-20 16:18:33 -0700

    Fix pycodestyle error

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
         user_id = int(sys.argv[1])
         url = "https://jsonplaceholder.typicode.com"
         user_resp = req.get('{}/users/{}'.format(url, user_id)).json()
-        todo_resp = req.get('{}/todos'.format(url), params={'userId': user_id}).json()
+        todo_resp = req.get('{}/todos'.format(url),
+                            params={'userId': user_id}).json()
         user_name = user_resp.get("username")
 
         with open('{}.csv'.format(user_id), 'w', newline='') as csvfile:
