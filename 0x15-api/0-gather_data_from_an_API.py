82137562a9f5917b812216ceb67a1abcd9e8f0a2
Adeboga Abigail <abbyboga@gmail.com>
2024-05-20 15:57:48 -0700

    Fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,13 +10,14 @@ if __name__ == '__main__':
         url = "https://jsonplaceholder.typicode.com"
         user_id = int(sys.argv[1])
         user_resp = req.get('{}/users/{}'.format(url, user_id)).json()
-        todo_resp = req.get('{}/todos'.format(url), params={'userId': user_id}).json()
+        todo_resp = req.get('{}/todos'.format(url),
+                            params={'userId': user_id}).json()
         completed_tasks = [title.get('title') for title in todo_resp
-                if title.get('completed') is True]
-        print('Employee {} is done with tasks {}/{}:'
-                .format(user_resp.get('name'),
-                    len(completed_tasks),
-                    len(todo_resp)))
+                           if title.get('completed') is True]
+        print('Employee {} is done with tasks({}/{}):'
+              .format(user_resp.get('name'),
+                      len(completed_tasks),
+                      len(todo_resp)))
         for task in completed_tasks:
             print('\t ' + task)
     except ValueError:
