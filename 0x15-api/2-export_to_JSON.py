f57328711ec3dfa71ed3265fa2b811b26bbeae65
Adeboga Abigail <abbyboga@gmail.com>
2024-05-20 16:28:35 -0700

    Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Export to JSON"""
+
+import json
+import requests as res
+import sys
+
+for __name__ == "__main__":
+    try:
+        user_id = int(sys.argv[1])
+        url = "https://jsonplaceholder.typicode.com"
+        user_resp = req.get('{}/users/{}'.format(url, user_id)).json()
+        user_name = user_resp.get('username')
+        todo_resp = req.get('{}/todos'.format(url),
+                            params={"userId": user_id}).json()
+
+        with open('{}.json'.format(user_id), 'w') as jsonfile:
+            json.dump({user_id: [{'task': todo.get('title'),
+                                  'completed': todo.get('completed'),
+                                  'username': user_name}
+                                 for todo in todo_resp]},
+                      jsonfile)
+
+    except ValueError:
+        pass
