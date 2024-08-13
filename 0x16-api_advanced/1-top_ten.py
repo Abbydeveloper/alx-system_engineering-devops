e545aca1991356947d4e8d49d9dc5e44fca2bc58
Adeboga Abigail <abbyboga@gmail.com>
2024-08-13 12:35:20 -0700

    Try again

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,40 +1,23 @@
 #!/usr/bin/python3
-"""Query the reddit API and print the titles of the first 10 hot posts list"""
+"""A function that queries Reddit API and prints the titles of the first 10
+hot posts listed for a given subreddit.
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """Get top ten host posts"""
-
-    if subreddit:
-        try:
-            limit = 10
-            url = "https://api.reddit.com/r/{:s}/hot.json".format(subreddit)
-            headers = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; \
-                Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
-                Chrome/111.0.0.0 Safari/537.36"
-                      }
-            params = {'after': None, "limit": limit}
-
-            response = requests.get(url, headers=headers, params=params,
-                                    allow_redirects=False)
-            response.raise_for_status()
-            response = response.json()
-
-            if 'data' in response and response['data']['children']:
-                posts = response['data']['children']
-
-                count = limit
-                for post in posts:
-                    count = count - 1
-                    print(post['data']['title'], end="")
-                    if count > 0:
-                        print("")
-
-            else:
-                print(None)
-        except Exception:
-            print(None)
-    else:
-        print(None)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Alt6242)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
