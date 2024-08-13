38f67001fab63bec41421e062d52ff34d6a9998c
Adeboga Abigail <abbyboga@gmail.com>
2024-08-13 13:40:39 -0700

    Fix?

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,40 +3,45 @@
 import requests
 
 
-def count_words(subreddit, word_list, after=[], word_count):
+def count_words(subreddit, word_list, after=[], word_count={}):
     """Print a sorted count of given keywords"""
 
-    url = "https://www.reddit.com/r/{:s}/hot.json".format(subreddit)
-    headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
-            }
-    params = {"after": after, "limit": 10}
+    try:
+        url = "https://www.reddit.com/r/{:s}/hot.json".format(subreddit)
+        headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+                }
+        params = {"after": after, "limit": 10}
 
-    if not word_count:
-        for word in word_list:
-            word_count.word = 0
-    if after is None:
-        word_list = [[key, value] for key, value in word_count.items()]
-        word_list = sorted(word_list, key=lambda x:(-x[1], x[0]))
-        for word in word_list:
-            if word[1]:
-                print('{}:{}'.format(word[0].lower(), word[1]))
-        return None
-
-
-    response = requests. get(url, params, headers, allow_redirects=False)
-    response = response.json()
-
-    if 'data' in response and response['data']['children']:
-        after = response['data']['after']
-        posts = response['data']['children']
-        for post in posts:
-            title = post.get('title')
-            lowercase = [s.lower() for s in title.split(' ')]
+        if not word_count:
             for word in word_list:
-                word_count[word] += lowercase.count(word.lower())
-    else:
-        return None
+                word_count[word] = 0
+        if after is None:
+            word_list = [[key, value] for key, value in word_count.items()]
+            word_list = sorted(word_list, key=lambda x:(-x[1], x[0]))
+            for word in word_list:
+                if word[1]:
+                    print('{}:{}'.format(word[0].lower(), word[1]))
+            return None
 
-    count_words(subreddit, word_list, after, word_count)
+        response = requests.get(url, params=params, headers=headers,
+                                allow_redirects=False)
+        response.raise_for_status()
+        response = response.json()
+
+        if 'data' in response and response['data']['children']:
+            after = response['data']['after']
+            posts = response['data']['children']
+            for post in posts:
+                title = post.get('title')
+                lowercase = [s.lower() for s in title.split(' ')]
+                for word in word_list:
+                    word_count[word] += lowercase.count(word.lower())
+        else:
+            return None
+
+        count_words(subreddit, word_list, after, word_count)
+
+    except requests.exceptions.RequestException:
+            return None
