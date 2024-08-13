936c752b8534b9423f7ad944c59adcca2009063a
Adeboga Abigail <abbyboga@gmail.com>
2024-08-13 12:15:51 -0700

    Edit top ten code with fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,23 +8,29 @@ def top_ten(subreddit):
 
     if subreddit:
         try:
+            limit = 10
             url = "https://api.reddit.com/r/{:s}/hot.json".format(subreddit)
             headers = {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; \
                 Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
                 Chrome/111.0.0.0 Safari/537.36"
                       }
-            params = {'after': None, "limit": 10}
+            params = {'after': None, "limit": limit}
 
             response = requests.get(url, headers=headers, params=params,
                                     allow_redirects=False)
             response.raise_for_status()
             response = response.json()
+
             if 'data' in response and response['data']['children']:
                 posts = response['data']['children']
 
+                count = limit
                 for post in posts:
-                    print(post['data']['title'])
+                    count = count - 1
+                    print(post['data']['title'], end="")
+                    if count > 0:
+                        print("")
 
             else:
                 print(None)
