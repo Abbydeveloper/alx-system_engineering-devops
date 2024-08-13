27447e8e07d7b06f7bdb9dd887dfbc7daa1ea8ba
Adeboga Abigail <abbyboga@gmail.com>
2024-08-13 09:31:11 -0700

    Fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
 """Query the reddit API and print the titles of the first 10 hot posts list"""
+import requests
 
 
 def top_ten(subreddit):
     """Get top ten host posts"""
 
-    url = "https://wwworeddit.com/r/{:s}/hot/.json".format(subreddit)
+    url = "https://www.reddit.com/r/{:s}/hot.json".format(subreddit)
     headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
             }
-    params = { "limit": 10}
+    params = {"limit": 10}
 
     try:
-        response = requests.get(url, headers=headers, params=params)
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False)
 
         if response.status_code == 200:
-            data = response.json()
-            return data
-        return None
+            data = response.json()['data']['children']
+
+            for post in data:
+                print(post['data']['title'])
+
+        else:
+            print(None)
     except (KeyError, ValueError):
-        return None
+        print(None)
