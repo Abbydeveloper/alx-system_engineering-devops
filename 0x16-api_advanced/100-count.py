49c626e05ae42a9f562733929adc5fcfd36cbc0a
Adeboga Abigail <abbyboga@gmail.com>
2024-08-13 13:44:08 -0700

    Fix?

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Query the Reddit API, parse the title of all hot articles and print a sorted count of given keywords"""
+"""Query the Reddit API, parse the title of all hot articles and
+print a sorted count of given keywords"""
 import requests
 
 
@@ -9,8 +10,9 @@ def count_words(subreddit, word_list, after=[], word_count={}):
     try:
         url = "https://www.reddit.com/r/{:s}/hot.json".format(subreddit)
         headers = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) \
+            Chrome/111.0.0.0 Safari/537.36"
                 }
         params = {"after": after, "limit": 10}
 
@@ -19,7 +21,7 @@ def count_words(subreddit, word_list, after=[], word_count={}):
                 word_count[word] = 0
         if after is None:
             word_list = [[key, value] for key, value in word_count.items()]
-            word_list = sorted(word_list, key=lambda x:(-x[1], x[0]))
+            word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
             for word in word_list:
                 if word[1]:
                     print('{}:{}'.format(word[0].lower(), word[1]))
@@ -44,4 +46,4 @@ def count_words(subreddit, word_list, after=[], word_count={}):
         count_words(subreddit, word_list, after, word_count)
 
     except requests.exceptions.RequestException:
-            return None
+        return None
